4d72b25aea048ef9f0a6e377bfb92ff56f4d0ce6
DESKTOP-S3G193N\const <constantine.lord.darkness@gmail.com>
2019-04-15 13:32:21 +0300

    - added animation popup

---
+++ UIPopUpLives/Assets/Scripts/UI/Popups/UIPopupBarLife.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class UIPopupBarLife : MonoBehaviour
+{
+    [Header("Reference Buttons")]
+    [SerializeField] private UIButton pointerButtonLifeBar;
+
+    void Start()
+    {
+        if (pointerButtonLifeBar != null)
+        {
+            EventDelegate.Set(pointerButtonLifeBar.onClick, delegate () { Show(); });
+        }
+    }
+
+    public void Show()
+    {
+        UIPopupLives.Current.Show();
+    }
+}
